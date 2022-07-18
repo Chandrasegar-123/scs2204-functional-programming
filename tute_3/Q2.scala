1871ae7717debec163c5bb60d35b24f4354668d0
Chandrasegar-123 <92800991+Chandrasegar-123@users.noreply.github.com>
2022-07-18 10:57:26 -0700

    Delete tute_3 directory

--- tute_3/Q2.scala
+++
@@ -1,8 +0,0 @@
-package tute_3
-
-object Q2 {
-  def main(args: Array[String]): Unit ={
-    println(Temp_to_fahrenheit(35))
-  }
-  def Temp_to_fahrenheit(c : Double) : Double = { c*1.8000+32.00}
-}
--- tute_3/Q5.scala
+++
@@ -1,8 +0,0 @@
-package tute_3
-
-object Q5 {
-  def main(args : Array[String]):Unit = {
-    println(Total_run_time())
-  }
-  def Total_run_time() = {2*8+3*7+2*8}
-}
